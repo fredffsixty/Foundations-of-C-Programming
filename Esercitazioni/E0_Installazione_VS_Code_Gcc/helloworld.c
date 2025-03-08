73ab931a522375f6818dad2b52995d1e7f80104a
fredffsixty <rpirrone@gmail.com>
2025-03-08 09:16:14 +0100

    Esercitazioni

---
+++ Esercitazioni/E0_Installazione_VS_Code_Gcc/helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    /* 
+        Il programma invia un messaggio di saluto
+    */
+    
+    printf("Ciao, sono il tuo compilatore C!\nTutto OK!\n");
+
+    return 0;
+}
